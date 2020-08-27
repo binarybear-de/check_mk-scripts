05786b36825562cca5b077eaec4eefeef191df73
BinaryBear <maxspangenberg@web.de>
2020-08-27 16:22:48 +0200

    Update check_apache2_certs.sh

--- check_apache2_certs.sh
+++ check_apache2_certs.sh
@@ -8,34 +8,22 @@
 #Set your limits here - currently defaults for till from Check_MK are used
 WARN_DAYS=28
 CRIT_DAYS=14
-
-# Loop over all enabled Site-Files from Apache2 with .conf suffix and skip broken SYMLINKS. Get the Path to the Certificate-File
-#for TARGET in $(cat /etc/apache2/sites-enabled/*.conf |
-#               grep -v "^\s*#"
-#               grep "SSLCertificateFile" | awk {'print $2'} |
-#               grep -v "No such file or directory")
-#do
+WARN_DAYS_DATE=$(($(date +%s) + (86400*$WARN_DAYS)));
+CRIT_DAYS_DATE=$(($(date +%s) + (86400*$CRIT_DAYS)));
 
 find /etc/apache2/sites-enabled/ -iname *.conf |
 while read FILE ; do
 	if [ -e "$FILE" ]; then
-		cat "$FILE" |
-			grep -v "^\s*#" |
-			grep "SSLCertificateFile" |
-			awk {'print $2'}
+		cat "$FILE" | grep -v "^\s*#" | grep "SSLCertificateFile" | awk {'print $2'}
 	fi
 done | while read TARGET; do
 
 	# Parse the Certifiate's lifespan
-	EXPIRATIONDATE=$(date -d "$(: | openssl x509 -in $TARGET -text -noout | grep 'Not After' |awk '{print $4,$5,$7}')" '+%s');
+	EXPIRATIONDATE=$(date -d "$(: | openssl x509 -in $TARGET -text -noout | grep 'Not After' | awk '{print $4,$5,$7}')" '+%s');
 	
 	# Get the Common name of the certificate
 	VHOST=$(openssl x509 -in $TARGET -text -noout | grep "Subject: CN" | awk {'print $4'})
-
-	# Set the limits in the needed format
-	WARN_DAYS_DATE=$(($(date +%s) + (86400*$WARN_DAYS)));
-	CRIT_DAYS_DATE=$(($(date +%s) + (86400*$CRIT_DAYS)));
-
+	
 	# Check if certificate is already expired
 	if [ $(date +%s) -gt $EXPIRATIONDATE ]; then
 		STATE=2 #Check_MK's Critical-STATE
