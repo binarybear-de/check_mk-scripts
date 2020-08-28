fb6cce26695de59ac4a93bd0236bdb9fa6065937
BinaryBear <maxspangenberg@web.de>
2020-08-28 16:25:03 +0200

    Update check_openwrt_wlan_clients.sh

--- check_openwrt_wlan_clients.sh
+++ check_openwrt_wlan_clients.sh
@@ -7,7 +7,7 @@
 # for dual band you may need to run this twice for each band to keep those client numbers separated...
 
 FILTER="wlan" #search-filter for autodetection, e.g. 'wlan' or 'ath' depending on your hardware
-NAME="wlan" #the check's name in Check_MK
+NAME="WLAN" #the check's name in Check_MK
 WARN_CLIENTS=6 #threshold for WARNING state
 CRIT_CLIENTS=8 #threshold for CRITIAL state
 STATE_DISABLED=0 #state if wifi is detected as disabled, 0 = OK, 1 = WARN, 2 = CRIT, 3 = UNKNOWN
@@ -15,15 +15,21 @@ STATE_DISABLED=0 #state if wifi is detected as disabled, 0 = OK, 1 = WARN, 2 = C
 ####
 
 CLIENTS=$(ifconfig | grep $FILTER | awk {'print $1'} | while read line ; do iw dev $line station dump | grep "Station" ; done | wc -l)
-if [ $CLIENTS -lt $WARN_CLIENTS ]; then STATE=0
-elif [ $CLIENTS -ge $WARN_CLIENTS ] && [ $CLIENTS -lt CRIT_CLIENTS ]; then STATE=1
-elif [ $CLIENTS -ge $CRIT_CLIENTS ]; then STATE=2
-else STATE=3
-fi
-if [ $(uci get wireless.radio0.disabled) == 1 ] ; then
-DESCRIPTION="WLAN is disabled!"
-STATE=$STATE_DISABLED
+if [ $CLIENTS -lt $WARN_CLIENTS ]; then
+	STATE=0
+elif [ $CLIENTS -ge $WARN_CLIENTS ] && [ $CLIENTS -lt CRIT_CLIENTS ]; then
+	STATE=1
+elif [ $CLIENTS -ge $CRIT_CLIENTS ]; then
+	STATE=2
 else
-DESCRIPTION="$(uci get wireless.radio0.txpower) dBm, $CLIENTS Clients: $(wlan l | awk {'print $2'} | tr '\n' ' ')"
+	STATE=3
 fi
+
+if [ $(uci get wireless.radio0.disabled) == 1 ] ; then
+	DESCRIPTION="WLAN is disabled!"
+	STATE=$STATE_DISABLED
+else
+	DESCRIPTION="$(uci get wireless.radio0.txpower) dBm, $CLIENTS Clients: $CLIENTS"
+fi
+
 echo "$STATE $NAME clients=$CLIENTS $DESCRIPTION"
