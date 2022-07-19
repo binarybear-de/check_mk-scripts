f213e054ef3d97ab6fbaeeff3530fabb9ced8aaf
BinaryBear <maxspangenberg@web.de>
2022-07-20 01:50:41 +0200

    Update check_certs_folder.sh

--- check_certs_folder.sh
+++ check_certs_folder.sh
@@ -21,39 +21,41 @@ STATE=0
 FOLDER=$(basename -- $1) # get folder path from complete path (used as name for the service in Check_MK)
 for TARGET in $1/*.crt; do
 
-FILENAME=$(basename -- $TARGET)
-# Parse the Certifiate
-expirationdate=$(date -d "$(: | openssl x509 -in $TARGET -text -noout | grep 'Not After' |awk '{print $4,$5,$7}')" '+%s');
+        FILENAME=$(basename -- $TARGET)
+        # Parse the Certifiate
+        expirationdate=$(date -d "$(: | openssl x509 -in $TARGET -text -noout | grep 'Not After' |awk '{print $4,$5,$7}')" '+%s');
 
-inwarndays=$(($(date +%s) + (86400*$WARN_DAYS)));
-incritdays=$(($(date +%s) + (86400*$CRIT_DAYS)));
+        inwarndays=$(($(date +%s) + (86400*$WARN_DAYS)));
+        incritdays=$(($(date +%s) + (86400*$CRIT_DAYS)));
 
 
-if [ $leastexpirationdate -gt $expirationdate ] || [ $leastexpirationdate -eq 0 ]; then
-leastexpirationdate=$expirationdate
-leastexpirationcert=$FILENAME
-fi
+        if [ $leastexpirationdate -gt $expirationdate ] || [ $leastexpirationdate -eq 0 ]; then
+                leastexpirationdate=$expirationdate
+                leastexpirationcert=$FILENAME
+        fi
 
-expirydays=$(( ($expirationdate-$(date +%s)) /86400))
-if [ $(date +%s) -gt $expirationdate ]; then
-STATE=2
-DESCRIPTION="$DESCRIPTION $FILENAME has already expired!"
+        expirydays=$(( ($expirationdate-$(date +%s)) /86400))
+        #echo "$TARGET $expirydays"
+        if [ $(date +%s) -gt $expirationdate ]; then
+                STATE=2
+                DESCRIPTION="$DESCRIPTION $FILENAME has already expired!"
 
-elif [ $incritdays -gt $expirationdate ]; then
-STATE=2
-DESCRIPTION="$DESCRIPTION $FILENAME expires in $expirydays Days,"
+        elif [ $incritdays -gt $expirationdate ]; then
+                STATE=2
+                DESCRIPTION="$DESCRIPTION $FILENAME expires in $expirydays Days,"
 
-elif [ $inwarndays -gt $expirationdate ]; then
-if [ $STATE -eq 0 ]; then STATE=1 fi #Set WARN state only if it was OKAY before to prevent overriding CRITICAL with WARNING
-DESCRIPTION="$DESCRIPTION $FILENAME expires in $expirydays Days,"
+        elif [ $inwarndays -gt $expirationdate ]; then
+                if [ $STATE -eq 0 ]; then STATE=1; fi #Set WARN state only if it was OKAY before to prevent overriding CRITICAL with WARNING
+                DESCRIPTION="$DESCRIPTION $FILENAME expires in $expirydays Days,"
 
-elif [ $inwarndays -lt $expirationdate ]; then
-: #Do nothing to preserve a 
+        elif [ $inwarndays -lt $expirationdate ]; then
+                :
 
-else #Set unknown and exit if output is garbage
-STATE=3
-exit
-fi;
+        else #Set unknown and exit if output is garbage
+                STATE=3
+                exit
+        fi
+
 done
 
 if [ $STATE -eq 0 ]; then
@@ -64,6 +66,6 @@ fi
 echo "$STATE SSL-Certs_$FOLDER - $DESCRIPTION"
 }
 
-# define you FOLDERs here - currently .crt-files only to exclude private keys
+# define you FOLDERs here - currently .crt-files only to exclude private keys if existing
 #check_certs_in_FOLDER "/some/path"
 #check_certs_in_FOLDER "/some/other/path"
